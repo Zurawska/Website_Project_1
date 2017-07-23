88bbeccd99bf413ab40369e82a902a3a74af416b
Edyta <edyta.zurawska8@gmail.com>
2017-07-23 15:06:16 +0200

    add plugin browser-sync and add task browserSync

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,21 @@ module.exports = function(grunt) {
                 spawn: false,
             },
         }
+    },
+
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src : [
+                    'css/*.css',
+                    '*.html'
+                ]
+            },
+            options: {
+                watchTask: true,
+                server: '.'
+            }
+        }
     }
 
   });
@@ -39,8 +54,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
 
   // Default task(s).
 
-  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+  grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
 };
