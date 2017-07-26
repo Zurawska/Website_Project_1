e2be67c70296cb0338cd4f562dc7358eb5881ef4
Edyta <edyta.zurawska8@gmail.com>
2017-07-26 19:35:02 +0200

    fix sources of imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
   			files: [{
   				expand: true,
   				cwd: 'images/',
-  				src: ['**/*.{png,jpg,jpeg,gif}'],
+  				src: ['*.{png,jpg,jpeg,gif}'],
   				dest: 'images/build/'
   			}]
   		}
